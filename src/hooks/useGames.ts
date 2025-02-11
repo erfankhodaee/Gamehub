3cf7549390d9e74cb71bb55ac369d8099ff3fc88
erfankhodaee <erfankhodaee2013@gmail.com>
2025-02-12 00:09:32 +0330

    Updated Sorting

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,7 +18,11 @@ export interface Game {
 
 const useGames = (gameQuery: GameQuery) =>
   useData<Game>("/games", [gameQuery], {
-    params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id },
+    params: {
+      genres: gameQuery.genre?.id,
+      platforms: gameQuery.platform?.id,
+      ordering: gameQuery.sortOrder,
+    },
   });
 
 export default useGames;
